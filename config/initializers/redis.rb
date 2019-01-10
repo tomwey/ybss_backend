9f6a735061970af18aefdb40529039bb74aba70d
tomwey <tomwey@163.com>
2019-01-10 12:25:50 +0800

    ccc

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,13 +4,13 @@ require 'redis/objects'
 
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 
-$redis = Redis.new(host: redis_config['host'], port: redis_config['port'], password: SiteConfig.redis_pass)
+$redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
 Redis::Objects.redis = $redis
 
-sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/1"
+sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/668"
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'sidekiq', url: sidekiq_url, password: SiteConfig.redis_pass }
+  config.redis = { namespace: 'sidekiq', url: sidekiq_url }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'sidekiq', url: sidekiq_url, password: SiteConfig.redis_pass }
+  config.redis = { namespace: 'sidekiq', url: sidekiq_url }
 end
