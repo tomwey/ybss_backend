2f52a542c9426e8df78ead0c950b093310aaed02
tomwey <tomwey@163.com>
2019-01-12 13:03:04 +0800

    ccc

--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -16,25 +16,25 @@ class CoverImageUploader < BaseUploader
     process resize_to_fill: [200, 356]
   end
   
-  # def filename
-  #   if original_filename.present?
-  #     # "#{SecureRandom.uuid}.#{file.extension}"
-  #     # current_path 是 Carrierwave 上传过程临时创建的一个文件，有时间标记
-  #     # 例如: /Users/jason/work/ruby-china/public/uploads/tmp/20131105-1057-46664-5614/_____2013-11-05___10.37.50.png
-  #     @name ||= Digest::MD5.hexdigest(original_filename)
-  #     "#{@name}.#{file.extension.downcase}"
-  #   end
-  # end
+  def filename
+    if original_filename.present?
+      # "#{SecureRandom.uuid}.#{file.extension}"
+      # current_path 是 Carrierwave 上传过程临时创建的一个文件，有时间标记
+      # 例如: /Users/jason/work/ruby-china/public/uploads/tmp/20131105-1057-46664-5614/_____2013-11-05___10.37.50.png
+      @name ||= Digest::MD5.hexdigest(original_filename)
+      "#{@name}.#{file.extension.downcase}"
+    end
+  end
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
-  def filename
-    if super.present?
-      "#{secure_token}.#{file.extension}"
-    end
-  end
+  # def filename
+  #   if super.present?
+  #     "#{secure_token}.#{file.extension}"
+  #   end
+  # end
   
   def extension_white_list
     %w(jpg jpeg png webp)
